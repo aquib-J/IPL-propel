1d3fa97dd01c9eea0f7f71d25760ac0f7892c35d
aquib-J <aquib.jansher@gmail.com>
2020-07-08 03:34:38 +0530

    visual skeleton set-up

--- public/app.js
+++ public/app.js
@@ -57,3 +57,219 @@ function visualizeMatchesPlayedPerYear(matchesPlayedPerYear) {
     ]
   });
 }
+
+// visual chart - 2
+
+Highcharts.chart("matches-won-by-each-time-alltime", {
+  chart: {
+      type: 'column'
+  },
+  title: {
+      text: '2. Number of matches won by each team over all the years of IPL'
+  },
+  subtitle: {
+      text: 'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
+  },
+  xAxis: {
+      categories: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec'
+      ],
+      crosshair: true
+  },
+  yAxis: {
+      min: 0,
+      title: {
+          text: 'Matches Won'
+      }
+  },
+  tooltip: {
+      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+      pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+          '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+      footerFormat: '</table>',
+      shared: true,
+      useHTML: true
+  },
+  plotOptions: {
+      column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+      }
+  },
+  series: [{
+      name: 'Tokyo',
+      data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+  }, {
+      name: 'New York',
+      data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+  }, {
+      name: 'London',
+      data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+
+  }, {
+      name: 'Berlin',
+      data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+
+  }]
+});
+
+// visual chart-3
+
+Highcharts.chart("extra-run-conceeded-by-each-team-2016", {
+  chart: {
+      type: 'column'
+  },
+  title: {
+    text: "3. Extra runs conceded by each team in 2016"
+  },
+  subtitle: {
+    text: 'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
+  },
+  xAxis: {
+      type: 'category',
+      labels: {
+          rotation: -45,
+          style: {
+              fontSize: '13px',
+              fontFamily: 'Verdana, sans-serif'
+          }
+      }
+  },
+  yAxis: {
+      min: 0,
+      title: {
+        text: "Extra Runs"
+      }
+  },
+  legend: {
+      enabled: false
+  },
+  tooltip: {
+      pointFormat: 'Population in 2017: <b>{point.y:.1f} millions</b>'
+  },
+  series: [{
+      name: 'Population',
+      data: [
+          ['Shanghai', 24.2],
+          ['Beijing', 20.8],
+          ['Karachi', 14.9],
+          ['Shenzhen', 13.7],
+          ['Guangzhou', 13.1],
+          ['Istanbul', 12.7],
+          ['Mumbai', 12.4],
+          ['Moscow', 12.2],
+          ['São Paulo', 12.0],
+          ['Delhi', 11.7],
+          ['Kinshasa', 11.5],
+          ['Tianjin', 11.2],
+          ['Lahore', 11.1],
+          ['Jakarta', 10.6],
+          ['Dongguan', 10.6],
+          ['Lagos', 10.6],
+          ['Bengaluru', 10.3],
+          ['Seoul', 9.8],
+          ['Foshan', 9.3],
+          ['Tokyo', 9.3]
+      ],
+      dataLabels: {
+          enabled: true,
+          rotation: -90,
+          color: '#FFFFFF',
+          align: 'right',
+          format: '{point.y:.1f}', // one decimal
+          y: 10, // 10 pixels down from the top
+          style: {
+              fontSize: '13px',
+              fontFamily: 'Verdana, sans-serif'
+          }
+      }
+  }]
+});
+
+
+//visual chart-4
+
+
+Highcharts.chart("top-economical-bowlers-2015", {
+  chart: {
+      type: 'column'
+  },
+  title: {
+    text: "4. Top Economical Bowlers in 2015 season"
+  },
+  subtitle: {
+    text: 'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
+  },
+  xAxis: {
+      type: 'category',
+      labels: {
+          rotation: -45,
+          style: {
+              fontSize: '13px',
+              fontFamily: 'Verdana, sans-serif'
+          }
+      }
+  },
+  yAxis: {
+      min: 0,
+      title: {
+        text: "Economy"
+      }
+  },
+  legend: {
+      enabled: false
+  },
+  tooltip: {
+      pointFormat: 'Population in 2017: <b>{point.y:.1f} millions</b>'
+  },
+  series: [{
+      name: 'Population',
+      data: [
+          ['Shanghai', 24.2],
+          ['Beijing', 20.8],
+          ['Karachi', 14.9],
+          ['Shenzhen', 13.7],
+          ['Guangzhou', 13.1],
+          ['Istanbul', 12.7],
+          ['Mumbai', 12.4],
+          ['Moscow', 12.2],
+          ['São Paulo', 12.0],
+          ['Delhi', 11.7],
+          ['Kinshasa', 11.5],
+          ['Tianjin', 11.2],
+          ['Lahore', 11.1],
+          ['Jakarta', 10.6],
+          ['Dongguan', 10.6],
+          ['Lagos', 10.6],
+          ['Bengaluru', 10.3],
+          ['Seoul', 9.8],
+          ['Foshan', 9.3],
+          ['Tokyo', 9.3]
+      ],
+      dataLabels: {
+          enabled: true,
+          rotation: -90,
+          color: '#FFFFFF',
+          align: 'right',
+          format: '{point.y:.1f}', // one decimal
+          y: 10, // 10 pixels down from the top
+          style: {
+              fontSize: '13px',
+              fontFamily: 'Verdana, sans-serif'
+          }
+      }
+  }]
+});
